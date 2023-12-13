6ee1576c0ffb69f378d4accf162dca13aa974a44
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2023-12-12 23:18:27 -0300

    Test content

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -3,6 +3,9 @@ import "./index.css"
 function MainContent() {
     return (
         <section>
+            <h2>Sobre</h2>
+            <div className="divider"></div>
+            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus possimus, maxime error iste quisquam hic laudantium molestiae corrupti consequatur maiores? Nesciunt dignissimos vel delectus laboriosam animi voluptate a provident cum?</h3>
         </section>
     )
 }
