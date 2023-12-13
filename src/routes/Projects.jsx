aefa5e777337bf45ff0c328743ff8684be9f83fb
João Vitor Nascimento da Silva <joaovitornascimento1885@gmail.com>
2023-12-13 20:29:47 -0300

    Add Projects component

---
+++ src/routes/Projects.jsx
@@ -0,0 +1,13 @@
+import "../index.css"
+
+function Projects() {
+    return (
+        <section>
+            <h2>Projetos</h2>
+            <div className="divider"></div>
+            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus possimus, maxime error iste quisquam hic laudantium molestiae corrupti consequatur maiores? Nesciunt dignissimos vel delectus laboriosam animi voluptate a provident cum?</h3>
+        </section>
+    )
+}
+
+export default Projects
