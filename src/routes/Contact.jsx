18386696afbfa0bf703565aebc7504bad7991301
João Vitor Nascimento da Silva <joaovitornascimento1885@gmail.com>
2023-12-13 20:29:22 -0300

    Add Contact Component

---
+++ src/routes/Contact.jsx
@@ -0,0 +1,13 @@
+import "../index.css"
+
+function Contact() {
+    return (
+        <section>
+            <h2>Contato</h2>
+            <div className="divider"></div>
+            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus possimus, maxime error iste quisquam hic laudantium molestiae corrupti consequatur maiores? Nesciunt dignissimos vel delectus laboriosam animi voluptate a provident cum?</h3>
+        </section>
+    )
+}
+
+export default Contact
