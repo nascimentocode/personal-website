638814e5a14a73f564a3043a3afc364b9b91154a
João Vitor Nascimento da Silva <joaovitornascimento1885@gmail.com>
2023-12-14 21:33:30 -0300

    Building contact page

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -5,7 +5,12 @@ function Contact() {
         <section>
             <h2>Contato</h2>
             <div className="divider-content"></div>
-            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus possimus, maxime error iste quisquam hic laudantium molestiae corrupti consequatur maiores? Nesciunt dignissimos vel delectus laboriosam animi voluptate a provident cum?</h3>
+            <div id="form-contact">
+                <input type="text" placeholder="Nome" />
+                <input type="email" placeholder="Email"/>
+                <textarea placeholder="Mensagem"></textarea>
+                <button>Enviar </button>
+            </div>
         </section>
     )
 }
