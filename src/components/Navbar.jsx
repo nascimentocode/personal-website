f47024171ad71f56d05c8e6510d6de4a8598c8ea
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2023-12-18 02:45:44 -0300

    Fix Navbar page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,34 @@
 import { NavLink } from "react-router-dom"
+// import { useState } from "react"
+// import { TfiAlignRight } from "react-icons/tfi";
 
 import "../index.css"
 
 function Navbar() {
+    // const [showNavbar, setShowNavbar] = useState(false)
+
+    // const handleShowNavbar = () => {
+    //     setShowNavbar(!showNavbar)
+    // }
+
     return (
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/sobre">Sobre</NavLink>
-            <NavLink to="/curriculo">Currículo</NavLink>
-            <NavLink to="/projetos">Projetos</NavLink>
-            <NavLink to="/contato">Contato</NavLink>
-        </nav>
+        <header>
+            <nav>
+                <div>
+                    <h1>Logo</h1>
+                </div>
+                {/* <div id="menu-icon" onClick={handleShowNavbar}>
+                    <TfiAlignRight />
+                </div> */}
+                <div id="nav-link">
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/sobre">Sobre</NavLink>
+                    <NavLink to="/curriculo">Currículo</NavLink>
+                    <NavLink to="/projetos">Projetos</NavLink>
+                    <NavLink to="/contato">Contato</NavLink>
+                </div>
+            </nav>
+        </header>
     )
 }
 
