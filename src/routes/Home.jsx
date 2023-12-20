73c32a6bec303a5a9456dea845f23a0879e16ab3
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2023-12-20 03:06:11 -0300

    Fix: fixing html tags

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,7 +3,9 @@ function Home() {
         <section>
             <h2>Sobre este site</h2>
             <div className="divider-content"></div>
-            <h3>Seja bem-vindo ao meu espaço digital! Este é o lugar onde compartilho meus projetos, experiências e um pouco sobre mim. Aqui, você encontrará informações sobre meus projetos anteriores e meu currículo. Explore à vontade e não hesite em entrar em contato comigo para colaborações ou apenas para trocar ideias sobre o vasto mundo da programação.</h3>
+            <div className="section-content">
+                <p>Seja bem-vindo ao meu espaço digital! Este é o lugar onde compartilho meus projetos, experiências e um pouco sobre mim. Aqui, você encontrará informações sobre meus projetos anteriores e meu currículo. Explore à vontade e não hesite em entrar em contato comigo para colaborações ou apenas para trocar ideias sobre o vasto mundo da programação.</p>
+            </div>
         </section>
     )
 }
