4b6b9ee3733212a12311fb5b12d98ef364cdc39d
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2023-12-20 20:05:58 -0300

    Fix: add tag p in Home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,6 +5,7 @@ function Home() {
             <div className="divider-content"></div>
             <div className="section-content">
                 <p>Seja bem-vindo ao meu espaço digital! Este é o lugar onde compartilho meus projetos, experiências e um pouco sobre mim. Aqui, você encontrará informações sobre meus projetos anteriores e meu currículo. Explore à vontade e não hesite em entrar em contato comigo para colaborações ou apenas para trocar ideias sobre o vasto mundo da programação.</p>
+                <p>Fonte disponível <a href="https://github.com/nascimentocode/LP-React" target="_blank">aqui</a>.</p>
             </div>
         </section>
     )
