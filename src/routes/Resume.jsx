d59d739da3a83978864048fbbe63dd229fd584d4
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2023-12-20 03:03:27 -0300

    Building resume page

--- src/routes/Resume.jsx
+++ src/routes/Resume.jsx
@@ -1,11 +1,34 @@
 import "../index.css"
 
+import ProgressBar from "../components/ProgressBar"
+
 function Resume() {
     return (
         <section>
             <h2>Currículo</h2>
             <div className="divider-content"></div>
-            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus possimus, maxime error iste quisquam hic laudantium molestiae corrupti consequatur maiores? Nesciunt dignissimos vel delectus laboriosam animi voluptate a provident cum?</h3>
+            <div className="section-content">
+                <div id="education">
+                    <h3>Formação Acadêmica</h3>
+                    <div>
+                        <a href="https://unasp.br/" target="_blank">UNASP</a>
+                        <p>Ensino Médio / Técnico em Informática - Concluído em 2019</p>
+                    </div>
+                    <div>
+                        <a href="https://www.unip.br/" target="_blank">UNIP</a>
+                        <p>Ciências da Computação – Cursando atualmente 8º Semestre</p>
+                    </div>
+                </div>
+                <div id="skills">
+                    <h3>Skills</h3>
+                    <div>
+                        <ProgressBar bgcolor="red" progress="75" language="Java" level="3"/>
+                        <ProgressBar bgcolor="aqua" progress="25" language="React" level="1"/>
+                        <ProgressBar bgcolor="purple" progress="100" language="Python" level="4"/>
+                        <ProgressBar bgcolor="yellow" progress="50" language="JavaScript" level="2"/>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
