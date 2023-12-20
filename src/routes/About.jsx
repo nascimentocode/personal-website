1a3832836aae34dff5852d5ab1724a720c6cefb1
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2023-12-20 03:06:54 -0300

    Building about page

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -5,7 +5,15 @@ function About() {
         <section>
             <h2>Sobre mim</h2>
             <div className="divider-content"></div>
-            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus possimus, maxime error iste quisquam hic laudantium molestiae corrupti consequatur maiores? Nesciunt dignissimos vel delectus laboriosam animi voluptate a provident cum?</h3>
+            <div className="section-content">
+                <p>Olá! Sou um entusiasta da tecnologia de 21 anos apaixonado pela arte do desenvolvimento Back-End. Minha jornada nesse mundo fascinante começou lá em 2017, mas foi em 2020 que decidi mergulhar de cabeça nos estudos e na prática para aprimorar minhas habilidades.</p>
+                <p>Durante meu período como estudante de Técnico em Informática, tive a oportunidade de explorar várias linguagens, incluindo PHP, SQL, C#, e até me aventurar um pouco no universo do React-Native. Esse período foi fundamental para despertar meu interesse e direcionar meu caminho para a Ciência da Computação, na qual iniciei minha jornada acadêmica em julho de 2020.</p>
+                <p>Ao longo dessa trajetória acadêmica, tive a oportunidade de me aprofundar em linguagens como Python, Java, C++, SQL e Bash. No entanto, minha sede por conhecimento vai além do currículo acadêmico. Por minha própria vontade, mergulhei em estudos envolvendo Python, Java, Javascript, Typescript e Lua. A paixão por aprender é o que me impulsiona, pois acredito que o conhecimento é um oceano sem limites, e estou sempre ansioso para explorar novos horizontes.</p>
+                <p>Adoro desafios e a complexidade do desenvolvimento Back-End me fascina. Enxergo a programação não apenas como uma profissão, mas como uma forma de expressão criativa, onde cada linha de código é uma oportunidade de criar algo inovador e funcional.</p>
+                <p>Se tivesse que escolher uma linguagem favorita, seria Python. Sua simplicidade combinada com sua versatilidade me encantam, e sinto-me em casa quando estou codificando nessa linguagem.</p>
+                <p>Estou animado com as possibilidades que o futuro reserva e comprometido em continuar a minha jornada de aprendizado constante. Afinal, para mim, o conhecimento nunca é demais e o desenvolvimento é um processo contínuo e empolgante.</p>
+                <p>Sinta-se à vontade para explorar meu site e acompanhar minha evolução nesse mundo em constante transformação!</p>
+            </div>
         </section>
     )
 }
