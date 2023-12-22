f030d2269a6920dbbab70be73d05a2e1e5721542
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2023-12-22 19:27:22 -0300

    React router refresh problem

--- src/main.jsx
+++ src/main.jsx
@@ -43,6 +43,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router} basename="/personal-website" />
   </React.StrictMode>,
 )
