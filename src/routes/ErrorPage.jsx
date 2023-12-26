b54d0edaa11df31369637f5a28e55b5a757ae9dc
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2023-12-26 01:40:25 -0300

    Building ErrorPage

--- src/routes/ErrorPage.jsx
+++ src/routes/ErrorPage.jsx
@@ -1,9 +1,19 @@
+import { useRouteError } from "react-router-dom"
 import Navbar from "../components/Navbar"
 
 function ErrorPage() {
+    const error = useRouteError();
+
     return (
         <div>
             <Navbar />
+            <div>
+                <h1>Oops!</h1>
+                <p>Sorry, an unexpected error has occurred.</p>
+                <p>
+                    <i>{error.statusText || error.message}</i>
+                </p>
+            </div>
         </div>
     )
 }
