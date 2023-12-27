6cb0533a520f10f47cb7d05307e0980992093b1a
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2023-12-26 21:05:55 -0300

    Fix: changing NavLink content according to path

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,24 @@ import Navbar from "./components/Navbar"
 import Sidebar from "./components/Sidebar"
 
 import { Outlet } from "react-router-dom"
+import { useLocation } from 'react-router-dom';
+
+function changeSidebar() {
+  const location = useLocation();
+  console.log(location)
+  
+  if (location.pathname === '/sobre') {
+    return <Sidebar to="curriculo" textButton="Saber mais" />
+  }
+  return <Sidebar to="sobre" textButton="Sobre mim" />
+}
 
 function App() {
   return (
     <div>
       <Navbar />
       <main>
-        <Sidebar />
+        {changeSidebar()}
         <Outlet />
       </main>
     </div>
