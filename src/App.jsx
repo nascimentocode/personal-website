762ac588b3bb2038272a12084081374897159d61
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2023-12-26 21:16:40 -0300

    Fix: switching functions to ternary operator

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,14 @@ import Sidebar from "./components/Sidebar"
 import { Outlet } from "react-router-dom"
 import { useLocation } from 'react-router-dom';
 
-function changeSidebar() {
-  const location = useLocation();
-  console.log(location)
-  
-  if (location.pathname === '/sobre') {
-    return <Sidebar to="curriculo" textButton="Saber mais" />
-  }
-  return <Sidebar to="sobre" textButton="Sobre mim" />
-}
-
 function App() {
+  const location = useLocation()
+
   return (
     <div>
       <Navbar />
       <main>
-        {changeSidebar()}
+        {location.pathname === '/sobre' ? <Sidebar to="curriculo" textButton="Saber mais" /> : <Sidebar to="sobre" textButton="Sobre mim" />}
         <Outlet />
       </main>
     </div>
