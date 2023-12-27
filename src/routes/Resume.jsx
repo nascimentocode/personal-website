4b1e5530d8583cf38cbbd6da0b960272bc4a588c
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2023-12-26 21:03:01 -0300

    Add progress bar Typescript

--- src/routes/Resume.jsx
+++ src/routes/Resume.jsx
@@ -24,10 +24,11 @@ function Resume() {
                     <div>
                         <ProgressBar bgcolor="#00304D" progress="75" language="Python" level="3"/>
                         <ProgressBar bgcolor="#EB2025" progress="75" language="Java" level="3"/>
-                        <ProgressBar bgcolor="#DB7533" progress="50" language="SQL" level="3"/>
+                        <ProgressBar bgcolor="#DB7533" progress="75" language="SQL" level="3"/>
                         <ProgressBar bgcolor="#BD9E00" progress="50" language="JavaScript" level="2"/>
                         <ProgressBar bgcolor="#787CB4" progress="50" language="PHP" level="2"/>
-                        <ProgressBar bgcolor="#087EA4" progress="25" language="React" level="1"/>
+                        <ProgressBar bgcolor="#087EA4" progress="50" language="React" level="2"/>
+                        <ProgressBar bgcolor="#3178C6" progress="25" language="Typescript" level="1"/>
                         <ProgressBar bgcolor="#3f631e" progress="25" language="Django" level="1"/>
                         <ProgressBar bgcolor="black" progress="25" language="Flask" level="1"/>
                     </div>
