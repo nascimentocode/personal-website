e7178b3498247d599e8305ce5a3a997e109ee84f
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2024-02-01 03:44:05 -0300

    Fix: style CardProject

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -3,16 +3,19 @@ import "../index.css"
 function CardProject({link, title, data, img, desc}){
     return (
         <div id="card-project">
-            <div id="card-header">
-                <a href={link} target="_blank">{title}</a>
-                <p>{data}</p>
-            </div>
-            <div id="card-img">
-                <img src={img} alt="Tela da aplicação" />
-            </div>
-            <div id="card-description">
-                <p>{desc}.</p>
-            </div>
+            <a href={link}>
+            
+                <div id="card-header">
+                    <h2 href={link} target="_blank">{title}</h2>
+                    <p>{data}</p>
+                </div>
+                <div id="card-img">
+                    <img src={img} alt="Tela da aplicação" />
+                </div>
+                <div id="card-description">
+                    <p>{desc}.</p>
+                </div>
+            </a>
         </div>
     )
 }
