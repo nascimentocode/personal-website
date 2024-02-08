13157364943fa95fa83d5c82dbbe0d768f0118ae
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2024-02-08 02:36:03 -0300

    fix: Correcting ErrorPage

--- src/routes/ErrorPage.jsx
+++ src/routes/ErrorPage.jsx
@@ -7,13 +7,15 @@ function ErrorPage() {
     return (
         <div>
             <Navbar />
-            <div id="error">
-                <h1>Oops!</h1>
-                <p>Sorry, an unexpected error has occurred.</p>
-                <p>
-                    <i>{error.statusText || error.message}</i>
-                </p>
-            </div>
+            <main>
+                <div id="error">
+                    <h1>Oops!</h1>
+                    <p>Sorry, an unexpected error has occurred.</p>
+                    <p>
+                        <i>{error.statusText || error.message}</i>
+                    </p>
+                </div>
+            </main>
         </div>
     )
 }
