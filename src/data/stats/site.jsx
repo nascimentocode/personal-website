bec4d547fe5192dd2d2a0f143e137b907ace0925
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2024-02-15 01:12:59 -0300

    fix: Clean Code in the Stats Route

---
+++ src/data/stats/site.jsx
@@ -0,0 +1,20 @@
+const data = [
+    {
+        label: "Estrelas que este repositório tem no github",
+        key: "stargazers_count"
+    },
+    {
+        label: "Número de visualizações deste repositório",
+        key: "subscribers_count"
+    },
+    {
+        label: "Número de forks",
+        key: "forks"
+    },
+    {
+        label: "Ultima atualização em",
+        key: "pushed_at"
+    },
+]
+
+export default data
--- src/routes/Stats.jsx
+++ src/routes/Stats.jsx
@@ -1,27 +1,52 @@
-import data from '../data/stats'
+import { useState, useCallback, useEffect  } from 'react'
+import dataPersonal from '../data/stats/personal'
+import dataSite from '../data/stats/site'
 
-function Status(){
+function Stats(){
+    const [data, setData] = useState(dataSite)
+
+    const fetchData = useCallback(async () => {
+        const res = await fetch('https://api.github.com/repos/nascimentocode/personal-site')
+        const resData = await res.json()
+        console.log(resData)
+    })
+
+    useEffect(() => {
+        fetchData();
+    }, [fetchData]);
+
     return (
         <section>
             <h2>Status</h2>
             <div className="divider-content"></div>
             <div className="section-content">
-                <h3>Algumas estatísticas sobre mim</h3>
-                <div id="stats-table">
-                    <table>
-                        <tbody>
-                            {data.map((val) => (
-                                <tr>
-                                    <td width="70%">{val.label}</td>
-                                    <td>{val.value}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                <div>
+                    <h3>Algumas estatísticas sobre mim</h3>
+                    <div id="stats-personal-table">
+                        <table>
+                            <tbody>
+                                {dataPersonal.map((val) => (
+                                    <tr>
+                                        <td width="70%">{val.label}</td>
+                                        <td>{val.value}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div>
+                    <h3>Algumas estatísticas sobre este site</h3>
+                    <div id="stats-site-table">
+                        <table>
+                            <tbody>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </section>
     )
 }
 
-export default Status
+export default Stats
