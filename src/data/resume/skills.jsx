fdc1972946fa4c40cf8a4fd4b9706d664c82ad0d
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2024-02-15 01:13:10 -0300

    fix: Clean Code in the Resume Route

---
+++ src/data/resume/skills.jsx
@@ -0,0 +1,58 @@
+const data = [
+    {
+        bgcolor: "#00304D",
+        progress: 75,
+        language: "Python",
+        level: 3
+    },
+    {
+        bgcolor: "#EB2025",
+        progress: 75,
+        language: "Java",
+        level: 3
+    },
+    {
+        bgcolor: "#DB7533",
+        progress: 75,
+        language: "MySQL",
+        level: 3
+    },
+    {
+        bgcolor: "#BD9E00",
+        progress: 50,
+        language: "JavaScript",
+        level: 2
+    },
+    {
+        bgcolor: "#787CB4",
+        progress: 50,
+        language: "PHP",
+        level: 2
+    },
+    {
+        bgcolor: "#087EA4",
+        progress: 50,
+        language: "React",
+        level: 2
+    },
+    {
+        bgcolor: "#3178C6",
+        progress: 25,
+        language: "Typescript",
+        level: 1
+    },
+    {
+        bgcolor: "#3f631e",
+        progress: 25,
+        language: "Django",
+        level: 1
+    },
+    {
+        bgcolor: "black",
+        progress: 25,
+        language: "Flask",
+        level: 1
+    },
+]
+
+export default data
--- src/routes/Resume.jsx
+++ src/routes/Resume.jsx
@@ -1,6 +1,7 @@
 import "../index.css"
 
 import ProgressBar from "../components/ProgressBar"
+import data from "../data/resume/skills"
 
 function Resume() {
     return (
@@ -22,15 +23,7 @@ function Resume() {
                 <div id="skills">
                     <h3>Skills</h3>
                     <div>
-                        <ProgressBar bgcolor="#00304D" progress="75" language="Python" level="3"/>
-                        <ProgressBar bgcolor="#EB2025" progress="75" language="Java" level="3"/>
-                        <ProgressBar bgcolor="#DB7533" progress="75" language="MySQL" level="3"/>
-                        <ProgressBar bgcolor="#BD9E00" progress="50" language="JavaScript" level="2"/>
-                        <ProgressBar bgcolor="#787CB4" progress="50" language="PHP" level="2"/>
-                        <ProgressBar bgcolor="#087EA4" progress="50" language="React" level="2"/>
-                        <ProgressBar bgcolor="#3178C6" progress="25" language="Typescript" level="1"/>
-                        <ProgressBar bgcolor="#3f631e" progress="25" language="Django" level="1"/>
-                        <ProgressBar bgcolor="black" progress="25" language="Flask" level="1"/>
+                        {data.sort((a, b) => b.level - a.level).map((val) => <ProgressBar data={val} /> )}
                     </div>
                 </div>
             </div>
