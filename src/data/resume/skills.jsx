31dbcfb4a10f437a0a2c0c34f3c118805c5debbc
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2024-04-10 01:10:16 -0300

    feat: Adding category field to skills data

--- src/data/resume/skills.jsx
+++ src/data/resume/skills.jsx
@@ -1,57 +1,73 @@
 const data = [
     {
         bgcolor: "#00304D",
+        progress: 25,
+        language: "Bash",
+        level: 1,
+        category: ["ferramentas", "linguagens"]
+    },
+    {
+        bgcolor: "#00304D",
         progress: 75,
         language: "Python",
-        level: 3
+        level: 3,
+        category: ["linguagens", "python"]
     },
     {
         bgcolor: "#EB2025",
         progress: 75,
         language: "Java",
-        level: 3
+        level: 3,
+        category: ["linguagens"]
     },
     {
         bgcolor: "#DB7533",
         progress: 75,
         language: "MySQL",
-        level: 3
+        level: 3,
+        category: ["banco de dados", "linguagens"]
     },
     {
         bgcolor: "#BD9E00",
         progress: 50,
         language: "JavaScript",
-        level: 2
+        level: 2,
+        category: ["javascript", "linguagens", "desenvolvimento web"]
     },
     {
         bgcolor: "#787CB4",
         progress: 50,
         language: "PHP",
-        level: 2
+        level: 2,
+        category: ["langugages", "desenvolvimento web"]
     },
     {
         bgcolor: "#087EA4",
         progress: 50,
         language: "React",
-        level: 2
+        level: 2,
+        category: ["javascript", "desenvolvimento web"]
     },
     {
         bgcolor: "#3178C6",
         progress: 25,
         language: "Typescript",
-        level: 1
+        level: 1,
+        category: ["javascript", "desenvolvimento web", "linguagens"]
     },
     {
         bgcolor: "#3f631e",
         progress: 25,
         language: "Django",
-        level: 1
+        level: 1,
+        category: ["desenvolvimento web", "python"]
     },
     {
         bgcolor: "black",
         progress: 25,
         language: "Flask",
-        level: 1
+        level: 1,
+        category: ["desenvolvimento web", "python"]
     },
 ]
 
