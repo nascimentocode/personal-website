5d7cd0e07e3ec2bec6fd800c1cdeb99bf12304f0
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2024-04-12 06:05:31 -0300

    Changing the name of some attributes

--- src/data/resume/skills.jsx
+++ src/data/resume/skills.jsx
@@ -1,86 +1,75 @@
 const data = [
     {
         bgcolor: "#C3423F",
-        progress: 50,
-        language: "Git",
-        level: 2,
+        title: "Git",
+        competence: 2,
         category: ["ferramentas"]
     },
     {
         bgcolor: "#40494E",
-        progress: 50,
-        language: "SQLite",
-        level: 2,
+        title: "SQLite",
+        competence: 2,
         category: ["banco de dados", "desenvolvimento web"]
     },
     {
         bgcolor: "#689B68",
-        progress: 25,
-        language: "Bash",
-        level: 1,
+        title: "Bash",
+        competence: 1,
         category: ["ferramentas", "linguagens"]
     },
     {
         bgcolor: "#689B68",
-        progress: 75,
-        language: "Python",
-        level: 3,
+        title: "Python",
+        competence: 3,
         category: ["linguagens", "python"]
     },
     {
         bgcolor: "#689B68",
-        progress: 75,
-        language: "Java",
-        level: 3,
+        title: "Java",
+        competence: 3,
         category: ["linguagens"]
     },
     {
         bgcolor: "#40494E",
         progress: 75,
-        language: "MySQL",
-        level: 3,
+        title: "MySQL",
+        competence: 3,
         category: ["banco de dados", "desenvolvimento web"]
     },
     {
         bgcolor: "#515DD4",
-        progress: 50,
-        language: "JavaScript",
-        level: 2,
+        title: "JavaScript",
+        competence: 2,
         category: ["javascript", "linguagens", "desenvolvimento web"]
     },
     {
         bgcolor: "#689B68",
-        progress: 50,
-        language: "PHP",
-        level: 2,
+        title: "PHP",
+        competence: 2,
         category: ["langugages", "desenvolvimento web"]
     },
     {
         bgcolor: "#515DD4",
-        progress: 50,
-        language: "React",
-        level: 2,
+        title: "React",
+        competence: 2,
         category: ["javascript", "desenvolvimento web"]
     },
     {
         bgcolor: "#515DD4",
-        progress: 25,
-        language: "Typescript",
-        level: 1,
+        title: "Typescript",
+        competence: 2,
         category: ["javascript", "desenvolvimento web", "linguagens"]
     },
     {
         bgcolor: "#3896E2",
-        progress: 25,
-        language: "Django",
-        level: 1,
+        title: "Django",
+        competence: 1,
         category: ["desenvolvimento web", "python"]
     },
     {
         bgcolor: "#3896E2",
-        progress: 25,
-        language: "Flask",
-        level: 1,
+        title: "Flask",
+        competence: 1,
         category: ["desenvolvimento web", "python"]
     },
 ]
