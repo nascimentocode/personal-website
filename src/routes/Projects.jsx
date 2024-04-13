c68fd52fce68c87239bba7bfaafff6734fd2c07c
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2024-04-13 19:58:27 -0300

    feat: Filtering projects

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -1,15 +1,24 @@
 import "../index.css"
+import { useState } from "react"
 
 import CardProject from "../components/CardProject"
 import data from "../data/projects"
+import NavigationFilter from "../components/NavigationFilter"
 
 function Projects() {
+    const [selectedLanguage, setSelectedLanguage] = useState("todas")
+
+    const filteredProjects = selectedLanguage === "todas" ? data : data.filter(project => project.language.includes(selectedLanguage))
+
+    const filters = ["todas", "python", "java"]
+
     return (
         <section>
             <h2>Projetos</h2>
+            <NavigationFilter setSelectedFilters={setSelectedLanguage} selectedFilter={selectedLanguage} filters={filters} />
             <div className="divider-content"></div>
             <div className="section-content">
-                {data.map((val) => <CardProject data={val} /> )}
+                {filteredProjects.map((project, index) => <CardProject key={index} data={project} /> )}
             </div>
         </section>
     )
