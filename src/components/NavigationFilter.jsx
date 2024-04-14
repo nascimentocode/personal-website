f3a2c3960bcfcd266f7e8fb89595f6c8fef12b14
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2024-04-14 04:50:44 -0300

    feat: Defining dynamic styling

--- src/components/NavigationFilter.jsx
+++ src/components/NavigationFilter.jsx
@@ -1,8 +1,15 @@
+import { useLocation } from "react-router-dom"
 import "../index.css"
 
 function NavigationSkill({ setSelectedFilters, selectedFilter, filters }) {
+    const local = useLocation()
+    
+    var paddingStyle = "20px 0 0 0"
+    
+    if (local.pathname === "/curriculo") paddingStyle = "20px 0 5px 0"
+
     return(
-        <div id="navigation-skill">
+        <div id="navigation-skill" style={ {padding: paddingStyle} }>
             {filters.map((filter, index) => <a className={selectedFilter === filter ? 'active' : ''} key={index} onClick={() => setSelectedFilters(filter)}>{filter}</a>)}
         </div>
     )
