cfb5420952fd348f454290afc9287ca20c3b8565
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2024-05-04 05:03:15 -0300

    feat: Organizing the certificates with their titles and images

---
+++ src/data/resume/certificates.jsx
@@ -0,0 +1,42 @@
+export const certificates = [
+    {
+        title: "Python 3 Completo",
+        imageURL: "/personal-website/src/assets/certificates/udemyPython.jpg",
+    },
+    {
+        title: "Imersao DEV 7",
+        imageURL: "/personal-website/src/assets/certificates/aluraImersaoDev7.jpg",
+    },
+    {
+        title: "Discover",
+        imageURL: "/personal-website/src/assets/certificates/discover.jpg",
+    },
+    {
+        title: "Intensivao JavaScript",
+        imageURL: "/personal-website/src/assets/certificates/intensivaoJS.jpg",
+    },
+    {
+        title: "NWL Expert Python",
+        imageURL: "/personal-website/src/assets/certificates/NWL_Expert_Python.jpg",
+    },
+    {
+        title: "NWL Expert NodeJS",
+        imageURL: "/personal-website/src/assets/certificates/NWL_Expert_NodeJS.jpg",
+    },
+    {
+        title: "Jornada Python",
+        imageURL: "/personal-website/src/assets/certificates/Hashtagtreinamentos-Python.jpg",
+    },
+    {
+        title: "Python Na Prática",
+        imageURL: "/personal-website/src/assets/certificates/empowerdata.jpg",
+    },
+    {
+        title: "NWL Unite Python",
+        imageURL: "/personal-website/src/assets/certificates/NWL_Unite_Python.jpg",
+    },
+    {
+        title: "Pystack Week 10",
+        imageURL: "/personal-website/src/assets/certificates/pystack-week-10.png",
+    },
+]
