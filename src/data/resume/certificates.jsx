b4596e3408a7d04537fc582cf6baad9eb349192a
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2024-05-06 22:26:37 -0300

    fix: Placing the certificates in the public folder

--- src/data/resume/certificates.jsx
+++ src/data/resume/certificates.jsx
@@ -1,42 +1,42 @@
 export const certificates = [
     {
         title: "Python 3 Completo",
-        imageURL: "/personal-website/src/assets/certificates/udemyPython.jpg",
+        imageURL: "/personal-website/images/certificates/udemyPython.jpg",
     },
     {
         title: "Imersao DEV 7",
-        imageURL: "/personal-website/src/assets/certificates/aluraImersaoDev7.jpg",
+        imageURL: "/personal-website/images/certificates/aluraImersaoDev7.jpg",
     },
     {
         title: "Discover",
-        imageURL: "/personal-website/src/assets/certificates/discover.jpg",
+        imageURL: "/personal-website/images/certificates/discover.jpg",
     },
     {
         title: "Intensivao JavaScript",
-        imageURL: "/personal-website/src/assets/certificates/intensivaoJS.jpg",
+        imageURL: "/personal-website/images/certificates/intensivaoJS.jpg",
     },
     {
         title: "NWL Expert Python",
-        imageURL: "/personal-website/src/assets/certificates/NWL_Expert_Python.jpg",
+        imageURL: "/personal-website/images/certificates/NWL_Expert_Python.jpg",
     },
     {
         title: "NWL Expert NodeJS",
-        imageURL: "/personal-website/src/assets/certificates/NWL_Expert_NodeJS.jpg",
+        imageURL: "/personal-website/images/certificates/NWL_Expert_NodeJS.jpg",
     },
     {
         title: "Jornada Python",
-        imageURL: "/personal-website/src/assets/certificates/Hashtagtreinamentos-Python.jpg",
+        imageURL: "/personal-website/images/certificates/Hashtagtreinamentos-Python.jpg",
     },
     {
         title: "Python Na Prática",
-        imageURL: "/personal-website/src/assets/certificates/empowerdata.jpg",
+        imageURL: "/personal-website/images/certificates/empowerdata.jpg",
     },
     {
         title: "NWL Unite Python",
-        imageURL: "/personal-website/src/assets/certificates/NWL_Unite_Python.jpg",
+        imageURL: "/personal-website/images/certificates/NWL_Unite_Python.jpg",
     },
     {
         title: "Pystack Week 10",
-        imageURL: "/personal-website/src/assets/certificates/pystack-week-10.png",
+        imageURL: "/personal-website/images/certificates/pystack-week-10.png",
     },
 ]
