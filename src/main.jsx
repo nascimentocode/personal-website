36d9dcef3619741de5db0007813e14e5e0f9293d
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2024-05-09 03:27:25 -0300

    feat: Importing i18n configuration file

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import "./i18n/index.js"
 
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 
