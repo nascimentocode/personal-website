404f8db18418ebd1cf6d92f6cf7f5d6e52f73742
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2024-05-13 06:08:35 -0300

    feat: Adding the LanguageSwitcher component to the Navbar and doing the translations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,14 @@ import { NavLink } from "react-router-dom"
 import { useEffect, useState } from "react"
 import { FiAlignRight, FiX } from "react-icons/fi";
 
+import LanguageSwitcher from "./LanguageSwitcher";
+
 import "../index.css"
+import { useTranslation } from "react-i18next";
 
 function Navbar() {
+    const { t } = useTranslation('navbar')
+
     const [hamburguerOpen, setHamburguerOpen] = useState(false)
 
     const toggleHamburguer = () => {
@@ -37,17 +42,18 @@ function Navbar() {
         <header>
             <nav>
                 <div id="brand">
-                    <NavLink to="/">Nascimento<span>code</span></NavLink>
+                    <NavLink to="/" onClick={scrollToTop}>Nascimento<span>code</span></NavLink>
                 </div>
                 <div id="icon" onClick={toggleHamburguer}>
                     {hamburguerOpen ? <FiX /> : <FiAlignRight />}
                 </div>
                 <div id="nav-link" style={{display: hamburguerOpen || window.innerWidth > 768 ? "flex" : "none"}} onClick={scrollToTop}>
-                    <NavLink to="sobre" onClick={toggleHamburguer}>Sobre</NavLink>
-                    <NavLink to="curriculo" onClick={toggleHamburguer}>Currículo</NavLink>
-                    <NavLink to="projetos" onClick={toggleHamburguer}>Projetos</NavLink>
-                    <NavLink to="status" onClick={toggleHamburguer}>Status</NavLink>
-                    <NavLink to="contato" onClick={toggleHamburguer}>Contato</NavLink>
+                    <NavLink to="sobre" onClick={toggleHamburguer}>{t('about')}</NavLink>
+                    <NavLink to="curriculo" onClick={toggleHamburguer}>{t('resume')}</NavLink>
+                    <NavLink to="projetos" onClick={toggleHamburguer}>{t('projects')}</NavLink>
+                    <NavLink to="status" onClick={toggleHamburguer}>{t('stats')}</NavLink>
+                    <NavLink to="contato" onClick={toggleHamburguer}>{t('contact')}</NavLink>
+                    <LanguageSwitcher />
                 </div>
             </nav>
         </header>
