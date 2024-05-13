54242e574adc650e01a004b52225d656e49f474c
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2024-05-13 06:28:00 -0300

    feat: Translating the Resume page and certificates

--- src/data/resume/certificates.jsx
+++ src/data/resume/certificates.jsx
@@ -1,42 +1,52 @@
-export const certificates = [
-    {
-        title: "Python 3 Completo",
-        imageURL: "/personal-website/images/certificates/udemyPython.jpg",
-    },
-    {
-        title: "Imersao DEV 7",
-        imageURL: "/personal-website/images/certificates/aluraImersaoDev7.jpg",
-    },
-    {
-        title: "Discover",
-        imageURL: "/personal-website/images/certificates/discover.jpg",
-    },
-    {
-        title: "Intensivao JavaScript",
-        imageURL: "/personal-website/images/certificates/intensivaoJS.jpg",
-    },
-    {
-        title: "NWL Expert Python",
-        imageURL: "/personal-website/images/certificates/NWL_Expert_Python.jpg",
-    },
-    {
-        title: "NWL Expert NodeJS",
-        imageURL: "/personal-website/images/certificates/NWL_Expert_NodeJS.jpg",
-    },
-    {
-        title: "Jornada Python",
-        imageURL: "/personal-website/images/certificates/Hashtagtreinamentos-Python.jpg",
-    },
-    {
-        title: "Python Na Prática",
-        imageURL: "/personal-website/images/certificates/empowerdata.jpg",
-    },
-    {
-        title: "NWL Unite Python",
-        imageURL: "/personal-website/images/certificates/NWL_Unite_Python.jpg",
-    },
-    {
-        title: "Pystack Week 10",
-        imageURL: "/personal-website/images/certificates/pystack-week-10.png",
-    },
-]
+import { useTranslation } from 'react-i18next';
+
+function getCertificates(){
+    const { t } = useTranslation('certificates')
+
+    const certificates = [
+        {
+            title: t('pythonUdemy'),
+            imageURL: "/personal-website/images/certificates/udemyPython.jpg",
+        },
+        {
+            title: t('imersao7Alura'),
+            imageURL: "/personal-website/images/certificates/aluraImersaoDev7.jpg",
+        },
+        {
+            title: "Discover",
+            imageURL: "/personal-website/images/certificates/discover.jpg",
+        },
+        {
+            title: t('intensaoJS'),
+            imageURL: "/personal-website/images/certificates/intensivaoJS.jpg",
+        },
+        {
+            title: "NWL Expert Python",
+            imageURL: "/personal-website/images/certificates/NWL_Expert_Python.jpg",
+        },
+        {
+            title: "NWL Expert NodeJS",
+            imageURL: "/personal-website/images/certificates/NWL_Expert_NodeJS.jpg",
+        },
+        {
+            title: t('pythonHashtag'),
+            imageURL: "/personal-website/images/certificates/Hashtagtreinamentos-Python.jpg",
+        },
+        {
+            title: t('pythonEmpowerdata'),
+            imageURL: "/personal-website/images/certificates/empowerdata.jpg",
+        },
+        {
+            title: "NWL Unite Python",
+            imageURL: "/personal-website/images/certificates/NWL_Unite_Python.jpg",
+        },
+        {
+            title: "Pystack Week 10",
+            imageURL: "/personal-website/images/certificates/pystack-week-10.png",
+        },
+    ]
+
+    return certificates
+}
+
+export default getCertificates
--- src/routes/Resume.jsx
+++ src/routes/Resume.jsx
@@ -6,40 +6,54 @@ import ProgressBar from "../components/ProgressBar"
 import Certificates from "../components/Certificates"
 
 import { skills } from "../data/resume/skills"
-import { certificates } from "../data/resume/certificates"
+import getCertificates from "../data/resume/certificates"
+import { useTranslation } from "react-i18next"
 
 function Resume() {
-    const [selectedCategory, setSelectedCategory] = useState("todas")
+    const { t } = useTranslation('resume') 
 
-    const filteredSkills = selectedCategory === "todas" ? skills : skills.filter(skill => skill.category.includes(selectedCategory))
+    const allFilterId = "all"
+    const filters = [
+        { id: allFilterId, text: t('filterAll') },
+        { id: "database", text: t('databaseSkill') },
+        { id: "javascript", text: "JavaScript" },
+        { id: "languages", text: t('languagesSkill') },
+        { id: "python", text: "Python" },
+        { id: "tools", text: t('toolsSkill') },
+        { id: "webdev", text: t('webdevSkill') }
+    ] 
 
-    const filters = ["todas", "banco de dados", "javascript", "linguagens", "python", "ferramentas", "desenvolvimento web"]
+    const [selectedCategory, setSelectedCategory] = useState(allFilterId)
+
+    const filteredSkills = selectedCategory === allFilterId ? skills : skills.filter(skill => skill.category.includes(selectedCategory))
+    
+    const certificates = getCertificates()
 
     return (
         <section>
-            <h2>Currículo</h2>
+            <h2>{t('topic')}</h2>
             <div className="divider-content"></div>
             <div className="section-content">
                 <div id="education">
-                    <h3>Formação Acadêmica</h3> 
+                    <h3>{t('titleEducation')}</h3> 
                     <div>
                         <a href="https://unasp.br/" target="_blank">UNASP</a>
-                        <p>Ensino Médio / Técnico em Informática - Concluído em 2019</p>
+                        <p>{t('academicEducation1')}</p>
                     </div>
                     <div>
                         <a href="https://www.unip.br/" target="_blank">UNIP</a>
-                        <p>Ciências da Computação – Cursando atualmente 8º Semestre</p>
+                        <p>{t('academicEducation2')}</p>
                     </div>
                 </div>
                 <div id="skills">
-                    <h3>Skills</h3>
+                    <h3>{t('titleSkills')}</h3>
                     <NavigationFilter setSelectedFilters={setSelectedCategory} selectedFilter={selectedCategory} filters={filters} />
                     <div>
                     {filteredSkills.sort((a, b) => b.competence - a.competence || a.title.localeCompare(b.title)).map((skill, index) => <ProgressBar key={index} data={skill} /> )}
                     </div>
                 </div>
                 <div id="certificates">
-                    <h3>Certificados</h3>
+                    <h3>{t('titleCertificates')}</h3>
                     <div>
                         <ul>
                             {certificates.map((certificate, index) => <Certificates key={certificate.title} title={certificate.title} imageURL={certificate.imageURL} last={index === certificates.length - 1} />)}
