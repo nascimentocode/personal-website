b683f9e0e7975c37b1cb15ad05b219055fc656b7
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2024-05-13 06:32:27 -0300

    feat: Translating the Contact page

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,16 +1,18 @@
-import { useState } from "react"
 import "../index.css"
 
+import { useTranslation } from "react-i18next"
+
 function Contact() {
+    const { t } = useTranslation('contact')
     return (
         <section>
-            <h2>Contato</h2>
+            <h2>{t('topic')}</h2>
             <div className="divider-content"></div>
             <form action="https://formspree.io/f/xpzvaerj" method="POST" id="form-contact">
-                <input type="text" name="name" placeholder="Nome" required />
-                <input type="email" name="email" placeholder="Email" required />
-                <textarea name="message" placeholder="Mensagem" required></textarea>
-                <button type="submit">Enviar</button>
+                <input type="text" name="name" placeholder={t('placeholderName')} required />
+                <input type="email" name="email" placeholder={t('placeholderEmail')} required />
+                <textarea name="message" placeholder={t('placeholderMessage')} required></textarea>
+                <button type="submit">{t('button')}</button>
             </form>
         </section>
     )
