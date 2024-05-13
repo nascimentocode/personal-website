8312acd0736cc18700808c53fb3f46b2f0e26c4e
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2024-05-13 06:23:54 -0300

    feat: Translating the home page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,15 @@
+import { useTranslation } from "react-i18next"
+
 function Home() {
+    const { t } = useTranslation('homepage')
+    
     return (
         <section>
-            <h2>Sobre este site</h2>
+            <h2>{t('topic')}</h2>
             <div className="divider-content"></div>
             <div className="section-content">
-                <p>Seja bem-vindo ao meu espaço digital! Este é o lugar onde compartilho meus projetos, experiências e um pouco sobre mim. Aqui, você encontrará informações sobre meus projetos anteriores e meu currículo. Explore à vontade e não hesite em entrar em contato comigo para colaborações ou apenas para trocar ideias sobre o vasto mundo da programação.</p>
-                <p>Fonte disponível <a href="https://github.com/nascimentocode/LP-React" target="_blank">aqui</a>.</p>
+                <p>{t('welcome')}</p>
+                <p>{t('source')} <a href="https://github.com/nascimentocode/personal-website" target="_blank">{t('linkText')}</a>.</p>
             </div>
         </section>
     )
